dd2d2e4b77d8ca5e048b5ea80ad80d8f7065becf
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-11 16:10:32 -0600

    initial commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = ($"{answer} is less than 9");
+            }
+            else
+            {
+                response = ($"{answer} is greater than or equal to nine");
+            }
+        }
+    }
+}
